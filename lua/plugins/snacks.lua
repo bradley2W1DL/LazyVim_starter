ab74485d6cb9a04b9a4aba74dd49146b08e1ad1f
bradley2w1dl <bberger@joinhomebase.com>
2025-11-08 22:15:08 -0700

    First Pass at custom dashboard

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -76,7 +76,26 @@ return {
         },
       },
     },
-    -- dashboard = {}
+    dashboard = {
+      sections = {
+        {
+          section = "terminal",
+          -- render the current Omarchy background
+          -- cmd = "chafa ~/.config/omarchy/current/background --format symbols --symbols vhalf --size 60x17 --stretch; sleep .1",
+          -- Render the Great Wave Off Kanagawa - Hokusai
+          -- cmd = "chafa ~/.config/nvim/extras/kanagawa.jpg --format symbols --symbols vhalf --size 60x17 --stretch; sleep .1",
+          -- Live bonsai tree
+          cmd = "cbonsai -li -w 3.0 --leaf=&, --message=NEOVIM", -- TODO, get this to match the theme
+          height = 17,
+          padding = 1,
+        },
+        {
+          pane = 2,
+          { section = "keys", gap = 1, padding = 1 },
+          { section = "startup" },
+        },
+      },
+    },
   },
 }
 
